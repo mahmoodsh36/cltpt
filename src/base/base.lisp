3dd5de9b0f4b831c4b60be5f1f289ae665e1ac09
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-06-27 02:02:43 +0300

    make org-emph, org-italic, etc, convert properly

--- src/base/base.lisp
+++ src/base/base.lisp
@@ -4,16 +4,24 @@
    :parse :make-text-format
    :begin-of-line :end-of-line
    :text-format-text-object-types :text-format-by-name
-   :convert-file :text-macro :post-lexer-text-macro :text-object
-   :text-object-property :text-object-text
-   :text-object-init :region-begin
-   :region-end :text-object-convert
-   :pcase :make-region :text-object-finalize :text-object-parent :text-object-next-sibling
+   :convert-file :text-macro :post-lexer-text-macro
+   :pcase :make-region
    :mapcar-forest :convert-tree :text-format-escape :replace-chars-and-escapes
    :wrap-contents-for-convert
-   :document :map-text-object
+
+   :document
+   :text-object
+   :region-end :region-begin :region-length
+   :text-object-convert
+   :map-text-object
+   :text-object-begin :text-object-end
+   :text-object-finalize :text-object-parent :text-object-next-sibling
+   :text-object-prev-sibling :text-object-contents
+   :text-object-property :text-object-text :text-object-text-region
+   :text-object-init
+
    :find-submatch
-   :region-text :text-object-prev-sibling :text-object-contents))
+   :region-text))
 
 (in-package :cltpt/base)
 
