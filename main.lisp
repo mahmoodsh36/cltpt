8272492ae96ebe0b4c81581304b448fd0a5f17ba
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-09 21:49:45 +0300

    turn commandline functionality into a "feature"

--- main.lisp
+++ main.lisp
@@ -1,7 +1,9 @@
 (require 'asdf)
 (require 'uiop)
+(pushnew :commandline *features*)
 (pushnew #p"./" asdf:*central-registry* :test #'equal)
 (asdf:load-system "cltpt" :force t) ;; force recompilation
 ;; (asdf:load-system "cltpt")
-(in-package :cltpt)
+;; (in-package :cltpt)
+#+commandline
 (cltpt/commandline:commandline-main (uiop:command-line-arguments))
