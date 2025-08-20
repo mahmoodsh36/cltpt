6a279bbe3941f078c7922c9f9599b9db83a651da
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-08-20 14:39:29 +0300

    introduce tree.lisp for generic tree traversal/operations

--- src/combinator/utils.lisp
+++ src/combinator/utils.lisp
@@ -4,38 +4,9 @@
 
 (in-package :cltpt/combinator/utils)
 
-(defun tree-mapcons (node func)
-  "we iterate through the tree one subtree at a time and run FUNC on each.
-children are handled first."
-  (when (consp node)
-    (progn
-      (loop for child in (cdr node)
-            do (tree-mapcons child func))
-      (funcall func node))))
-
-(defun tree-find (node item &key (test #'equal) (key #'identity))
-  "find `ITEM' from the `car' of some node in the tree NODE.
-TEST checks for equality between ITEM and `(key (car node))'."
-  (tree-mapcons
-   node
-   (lambda (other-node)
-     (when (funcall test item (funcall key other-node))
-       (return-from tree-find other-node))))
-  nil)
-
-(defun tree-find-all (node item &key (test #'equal) (key #'identity))
-  "similar to `tree-find' but returns all instances matched from the tree."
-  (let ((result))
-    (tree-mapcons
-     node
-     (lambda (other-node)
-       (when (funcall test item (funcall key other-node))
-         (push other-node result))))
-    result))
-
 (defun find-submatch (match submatch-id)
   "from a combinator-returned MATCH, find a sub-match by its SUBMATCH-ID."
-  (tree-find
+  (cltpt/tree:tree-find
    match
    submatch-id
    :key (lambda (node)
@@ -45,7 +16,7 @@ TEST checks for equality between ITEM and `(key (car node))'."
 (defun find-submatch-last (match submatch-id)
   "from a combinator-returned MATCH, find a sub-match by its SUBMATCH-ID. return the last such submatch found."
   (let ((last-found))
-    (tree-mapcons
+    (cltpt/tree:tree-map
      match
      (lambda (submatch)
        (when (equal submatch-id (getf (car submatch) :id))
@@ -54,7 +25,7 @@ TEST checks for equality between ITEM and `(key (car node))'."
 
 (defun find-submatch-all (match submatch-id)
   "similar to `find-submatch', but returns all matches."
-  (tree-find-all
+  (cltpt/tree:tree-find-all
    match
    submatch-id
    :key (lambda (node)
---
+++ src/tree.lisp
@@ -0,0 +1,65 @@
+(defpackage :cltpt/tree
+  (:use :cl)
+  (:export :tree-value :tree-children :tree-map :tree-find-all :tree-find))
+
+(in-package :cltpt/tree)
+
+;; a module that abstracts functionality of different kinds of trees
+
+(defgeneric tree-value (subtree)
+  (:documentation "given a (sub)tree, return the value associated with its root.
+
+for a tree represented by a cons 'x', the 'value' might be '(car x)', while
+the children are represented by '(cdr x)'. for other structures, this may
+differ. we differentiate between the subtree (also called node) itself
+and its value using the generic 'tree-value' function, which takes a subtree
+and returns the value associated with the node at its root."))
+
+(defgeneric tree-children (subtree)
+  (:documentation "given a (sub)tree (better thought of as a node), return its children. this shouldnt be recursive."))
+
+(defmethod tree-value ((subtree cons))
+  (car subtree))
+
+(defmethod tree-children ((subtree cons))
+  (cdr subtree))
+
+(defmethod tree-map (subtree func)
+  "we iterate through the tree one subtree at a time and run FUNC on each.
+
+children are handled first. if ON-VALUE is `t', we run the function on the values
+rather than the subtrees (nodes) themselves."
+  (if (consp subtree)
+      (progn
+        (loop for child in (tree-children subtree)
+              do (when (consp child)
+                   (tree-map child func)))
+        (funcall func subtree))
+      subtree))
+
+(defmethod tree-find (subtree item
+                      &key (test #'equal) (key #'identity))
+  "find `ITEM' from in `SUBTREE'.
+
+TEST checks for equality between ITEM and `(key SUBTREE)'."
+  (tree-map
+   subtree
+   (lambda (other-subtree)
+     (when (funcall test
+                    item
+                    (funcall key other-subtree))
+       (return-from tree-find other-subtree))))
+  nil)
+
+(defun tree-find-all (subtree item
+                      &key (test #'equal) (key #'identity))
+  "similar to `tree-find' but returns all instances matched from the tree."
+  (let ((result))
+    (tree-map
+     subtree
+     (lambda (other-subtree)
+       (let* ((value other-subtree)
+              (value2 (funcall key value)))
+         (when (funcall test item value2)
+           (push value2 result)))))
+    result))
