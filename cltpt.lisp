5d9408c618e38b64fc8ab5230547e1d881786126
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-09-02 22:49:00 +0300

    improve handling of post-lexer macros in html templates

--- cltpt.lisp
+++ cltpt.lisp
@@ -9,3 +9,7 @@
         :parse nil
         :roam nil)
   "different debugging settings, if some property is set to `t', some corresponding debugging functionality throughout the source code will be activated.")
+
+(defvar *author*
+  nil
+  "author name used when generating a document.")
