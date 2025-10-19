e5f742b88435bcbfda6ecf902a21e7ab2a879149
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-10-19 21:12:26 +0300

    rewrite find-submatch which caused problems

--- src/combinator/utils.lisp
+++ src/combinator/utils.lisp
@@ -5,14 +5,26 @@
 
 (in-package :cltpt/combinator/utils)
 
-(defun find-submatch (match submatch-id &optional (test 'string=))
+;; (defun find-submatch (match submatch-id &optional (test 'string=))
+;;   "from a combinator-returned MATCH, find a sub-match by its SUBMATCH-ID."
+;;   (cltpt/tree:tree-find
+;;    match
+;;    submatch-id
+;;    :key (lambda (node)
+;;           (getf (car node) :id))
+;;    :test test))
+
+;; the other `find-submatch' isnt correct in some cases.
+;; TODO: figure out why the other find-submatch isnt correct.
+(defun find-submatch (match submatch-id)
   "from a combinator-returned MATCH, find a sub-match by its SUBMATCH-ID."
-  (cltpt/tree:tree-find
-   match
-   submatch-id
-   :key (lambda (node)
-          (getf (car node) :id))
-   :test test))
+  (when (and (consp (car match)) (symbolp (caar match)))
+    (when (string= (getf (car match) :id) submatch-id)
+      (return-from find-submatch match))
+    (loop for child in (cdr match)
+          do (let ((result (find-submatch child submatch-id)))
+               (when result
+                 (return-from find-submatch result))))))
 
 ;; TODO: easy to optimize, we dont have to iterate through the whole tree to find the last instance
 (defun find-submatch-last (match submatch-id &optional (test 'string=))
