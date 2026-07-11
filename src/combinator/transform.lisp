454169ae1bef89536df615b68b640081ccb80f25
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2026-07-11 23:24:10 +0300

    remove "transform" functionality

--- src/combinator/transform.lisp
+++ src/combinator/transform.lisp
@@ -17,66 +17,8 @@
 ;; also, the transformer itself should return a reader aswell, one that it should fill up
 ;; continuously while reading from its input reader.
 
-;; rule is a combinator rule, replacements is a list of plists to replace inner rules from the
-;; original RULE with.
-;; e.g. rule=(cltpt/combinator:separated-atleast-one ,(string #\newline) (:name myline :pattern (cltpt/combinator:all-but-newline)))
-;; replacements=((:name myline :replacement (:name mypaddedline :pattern '(lambda (reader writer match) (match-text reader match)))))
-(defun transform (reader match replacements)
-  (let ((full-rule (cltpt/combinator:match-rule match)))
-    ;; if the rule is a string its just a literal
-    (if (stringp full-rule)
-        full-rule
-        (let* ((rule (if (keywordp (car full-rule))
-                         (getf full-rule :pattern)
-                         full-rule))
-               (id (and (keywordp (car full-rule))
-                        (getf full-rule :id)))
-               (replacement (and id (cdr (assoc id replacements :test 'string=)))))
-          (when replacement
-            (setf rule
-                  (if (keywordp (car replacement))
-                      (getf replacement :pattern)
-                      replacement)))
-          (let* ((matcher (car rule))
-                 (transformer (transformer-for-combinator matcher)))
-            (or (and transformer
-                     (apply transformer
-                            reader
-                            (cdr rule)
-                            replacements
-                            (cltpt/combinator:match-children match)))
-                (cltpt/combinator:match-text match reader)))))))
-
-;; re-interning like that is probably not ideal, is there a better way to go about things?
-;; ideally we shouldnt have to implement the transformation functions ourselves, but have
-;; a process that goes through the original combinator and figures out how to transform the
-;; results based on the structure of the matching rule (e.g. we shouldnt have to implement
-;; 'transform-literal' explicitly.)
-(defun transformer-for-combinator (matcher)
-  (let ((func-name (intern (concatenate 'string "TRANSFORM-" (symbol-name matcher))
-                           (find-package :cltpt/transform))))
-    (when (fboundp func-name)
-      func-name)))
-
-(defun transform-consec (reader subrules replacements &rest matches)
-  (with-output-to-string (out)
-    (loop for match in matches
-          do (write-string (transform reader match replacements) out))
-    out))
-
-(defun transform-literal (reader subrules match)
-  (cltpt/combinator:match-text match reader))
-
-(defun transform-separated-atleast-one (reader subrules replacements &rest matches)
-  (with-output-to-string (out)
-    (loop for rest on matches
-          for match = (car rest)
-          do (write-string (transform reader match replacements) out)
-             (unless (null (cdr rest))
-               (write-char #\, out)))
-    out))
-
-;; this one simply extracts specific stuff from the match for embedding them in the new string
+;; this only works in cases where the original source rule that was used to parse the text matches
+;; the same structure of 'dest-rule'
 (defun reconstruct (reader match dest-rule)
   (if (stringp dest-rule)
       dest-rule
